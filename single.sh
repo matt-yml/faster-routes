ba495d592267f849f6eaab088ca59aa6ce5176b5
mattlesnake <matt.ym.lee@gmail.com>
2022-07-13 15:02:23 -0400

    cleaning up single script example

--- single.sh
+++ single.sh
@@ -2,11 +2,8 @@
 # Automate the routing process. Pass straight to sort
 # NOTE: Docker container that actually runs the scripts must already be built
 
-# Clean out raid channels
-#/usr/bin/docker run  -t --rm  purge -a raids.ini
-#docker run -it --rm -v /home/deve/pk/submodules/faster-routes:/usr/src/app -v /home/deve/pk/submodules/faster-routes/coords/z_utils/sp-craleigh.fence:/usr/src/app/fence.txt faster-routes
-#docker run -it --rm -v /home/deve/pk/submodules/faster-routes/route.txt:/usr/src/app/infile.txt sct python cluster.py -jsf infile.txt
-#cp route.txt /home/deve/pk/submodules/faster-routes/coords/z_utils/sp-craleigh.route
+# Copy the unknown spawn point config in
+cp /home/deve/pk/submodules/faster-routes/config_sp_ukn.json /home/deve/pk/submodules/faster-routes/config.json
 
 /usr/bin/docker run -it --rm -v /home/deve/pk/submodules/faster-routes:/usr/src/app -v /home/deve/pk/submodules/faster-routes/coords/z_utils/sp-westcamparks.fence:/usr/src/app/fence.txt faster-routes
 /usr/bin/docker run -it --rm -v /home/deve/pk/submodules/faster-routes/route.txt:/usr/src/app/infile.txt sct python cluster.py -jsf infile.txt
