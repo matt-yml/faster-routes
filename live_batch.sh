ca94da92f0ad0c68d561e6fa6570ec5c212528ce
mattlesnake <matt.ym.lee@gmail.com>
2022-07-13 22:59:16 -0400

    clean up live script. Add support for more areas. raw route output

--- live_batch.sh
+++ live_batch.sh
@@ -2,40 +2,31 @@
 # Automate the routing process. Pass straight to sort
 # NOTE: Docker container that actually runs the scripts must already be built
 
-## Sanity
-#i=0
-#j=0
-#if [ "$i" -eq "0" ];
-#then
-#  echo "i is 0"
-#  echo "j is $j"
-#else
-#  let "j++"
-#  echo "i is $i"
-#  echo "j is $j"
-#fi
-
 base_coords="/home/deve/pk/submodules/faster-routes/coords"
 base_fr="/home/deve/pk/submodules/faster-routes"
 
 echo "set config to known points only"
 cp ${base_fr}/config_sp_kn.json ${base_fr}/config.json
 
-area="cam"
-#fences=("bond" "cary_cbd" "northwoods" "parkwaypnt" "ritterpark" "silvercreek" "waverly")
-
+#area="cam"
 # Apex
 #fences=("apex_compark" "apex_dt" "danvers")
 # Cary
 #fences=("parkwaypnt" "ritterpark" "bond" "cary_cbd" "northwoods" "waverly")
 # CAM
-fences=("apex_compark" "apex_dt" "danvers" "parkwaypnt" "ritterpark" "bond" "cary_cbd" "northwoods" "waverly")
+#fences=("apex_compark" "apex_dt" "danvers" "parkwaypnt" "ritterpark" "bond" "cary_cbd" "northwoods" "waverly")
+
+
+# Raleigh CBD
+#fences=("ral_CBD")
+area="raleigh_NW"
+fences=("6forks" "crabtree" "glenwood" "neuse" "northhills" "statefair" "trinity")
 
 i=0
   for fence in ${!fences[@]}
     do
     echo "routing ${area}/${cities[$city]}/${fences[$fence]}"
-    /usr/bin/docker run -it --rm -v ${base_fr}:/usr/src/app -v ${base_coords}/${area}/${city}/${fences[$fence]}.fence:/usr/src/app/fence.txt faster-routes
+    /usr/bin/docker run -it --rm -v ${base_fr}:/usr/src/app -v ${base_coords}/${area}/${fences[$fence]}.fence:/usr/src/app/fence.txt faster-routes
     /usr/bin/docker run -it --rm -v ${base_fr}/route.txt:/usr/src/app/infile.txt sct python cluster.py -jsf infile.txt
     # Save individual route
     cp ${base_fr}/route.txt ${base_coords}/${area}/${city}/${fences[$fence]}.route
