f6a522b1c9eab06d053cc3fc30eb257bd4722649
mattlesnake <matt.ym.lee@gmail.com>
2022-07-20 10:54:04 -0400

    setup for local scripts to parallel tasks

---
+++ cam.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Automate the routing process. Pass straight to sort
+# NOTE: Docker container that actually runs the scripts must already be built
+
+## Copy the unknown spawn point config in
+#cp /home/deve/pk/submodules/faster-routes/config_sp_ukn.json /home/deve/pk/submodules/faster-routes/config.json
+#
+#/usr/bin/docker run -it --rm -v /home/deve/pk/submodules/faster-routes:/usr/src/app -v /home/deve/pk/submodules/faster-routes/coords/z_utils/sp-westcamparks.fence:/usr/src/app/fence.txt faster-routes
+#/usr/bin/docker run -it --rm -v /home/deve/pk/submodules/faster-routes/route.txt:/usr/src/app/infile.txt sct python cluster.py -jsf infile.txt
+#cp route.txt /home/deve/pk/submodules/faster-routes/coords/z_utils/sp-westcamparks.route
+#
+
+base_coords="/home/deve/devrepo/coords/maps/pk/volumes/rdm/coords"
+base_faster-routes="/home/deve/devrepo/coords/maps/pk/faster-routes"
+
+# Map desired config into container
+cfg=${base_faster-routes}/config_sp_ukn.json
+
+# Specify sub directory
+area=z_utils
+
+# Pick your fence.
+fence=sp-cam
+
+# Turn off the next line to just do sorting, like on a  blanket route
+/usr/bin/docker run -t --rm -v ${base_faster-routes}:/usr/src/app -v ${cfg}:/usr/src/app/config.json -v ${base_coords}/${area}/${fence}.fence:/usr/src/app/fence.txt faster-routes
+
+# Now sort
+/usr/bin/docker run -t --rm -v ${base_faster-routes}/route.txt:/usr/src/app/infile.txt sct python cluster.py -jsf infile.txt
+cp ${base_faster-routes}/route.txt ${base_faster-routes}/coords/${area}/${fence}.route
