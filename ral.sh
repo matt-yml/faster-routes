2101311a172de8bf01b2bb797e8c4d31a4ecd64d
mattlesnake <matt.ym.lee@gmail.com>
2022-07-24 10:21:08 -0400

    update batch script with raleigh placeholder and raleigh single script. also switch default to unknown points in single config.

--- ral.sh
+++ ral.sh
@@ -2,14 +2,6 @@
 # Automate the routing process. Pass straight to sort
 # NOTE: Docker container that actually runs the scripts must already be built
 
-## Copy the unknown spawn point config in
-#cp /home/deve/pk/submodules/faster-routes/config_sp_ukn.json /home/deve/pk/submodules/faster-routes/config.json
-#
-#/usr/bin/docker run -it --rm -v /home/deve/pk/submodules/faster-routes:/usr/src/app -v /home/deve/pk/submodules/faster-routes/coords/z_utils/sp-westcamparks.fence:/usr/src/app/fence.txt faster-routes
-#/usr/bin/docker run -it --rm -v /home/deve/pk/submodules/faster-routes/route.txt:/usr/src/app/infile.txt sct python cluster.py -jsf infile.txt
-#cp route.txt /home/deve/pk/submodules/faster-routes/coords/z_utils/sp-westcamparks.route
-#
-
 base_coords="/home/deve/devrepo/coords/maps/pk/volumes/rdm/coords"
 base_fr="/home/deve/devrepo/coords/submodules/faster-routes"
 
@@ -20,15 +12,11 @@ cfg=${base_fr}/config_sp_ukn.json
 area=z_utils
 
 # Pick your fence.
-fence=sp-durham
+fence=sp-raleigh
 
-echo "*****Starting ${fence}*****"
-date
 # Turn off the next line to just do sorting, like on a  blanket route
 /usr/bin/docker run -t --rm -v ${base_fr}:/usr/src/app -v ${cfg}:/usr/src/app/config.json -v ${base_coords}/${area}/${fence}.fence:/usr/src/app/fence.txt faster-routes
-date
+
 # Now sort
 /usr/bin/docker run -t --rm -v ${base_fr}/route.txt:/usr/src/app/infile.txt sct python cluster.py -jsf infile.txt
 cp ${base_fr}/route.txt ${base_fr}/coords/${area}/${fence}.route
-echo "*****Done with: ${fence}*****"
-date
