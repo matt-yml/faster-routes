a4ed55cda7be32623b926407e5af1f4ff8fad6a6
mattlesnake <matt.ym.lee@gmail.com>
2022-07-24 20:04:48 -0400

    add durham to regular spawn point optimization

--- batch.sh
+++ batch.sh
@@ -83,23 +83,23 @@ cp ${base_fr}/route.txt ${base_fr}/coords/${area}/${fence}.route
 echo "*****Done with: ${fence}*****"
 date
 
-#### durham
-## Map desired config into container
-#cfg=${base_fr}/config_sp_ukn.json
-## Specify sub directory
-#area=z_utils
-## Pick your fence
-#fence=sp-durham
-#echo "*****Starting ${fence}*****"
-#date
-## Turn off the next line to just do sorting, like on a  blanket route
-#/usr/bin/docker run -t --rm -v ${base_fr}:/usr/src/app -v ${cfg}:/usr/src/app/config.json -v ${base_coords}/${area}/${fence}.fence:/usr/src/app/fence.txt faster-routes
-#date
-## Now sort
-#/usr/bin/docker run -t --rm -v ${base_fr}/route.txt:/usr/src/app/infile.txt sct python cluster.py -jsf infile.txt
-#cp ${base_fr}/route.txt ${base_fr}/coords/${area}/${fence}.route
-#echo "*****Done with: ${fence}*****"
-#date
+### durham
+# Map desired config into container
+cfg=${base_fr}/config_sp_ukn.json
+# Specify sub directory
+area=z_utils
+# Pick your fence
+fence=sp-durham
+echo "*****Starting ${fence}*****"
+date
+# Turn off the next line to just do sorting, like on a  blanket route
+/usr/bin/docker run -t --rm -v ${base_fr}:/usr/src/app -v ${cfg}:/usr/src/app/config.json -v ${base_coords}/${area}/${fence}.fence:/usr/src/app/fence.txt faster-routes
+date
+# Now sort
+/usr/bin/docker run -t --rm -v ${base_fr}/route.txt:/usr/src/app/infile.txt sct python cluster.py -jsf infile.txt
+cp ${base_fr}/route.txt ${base_fr}/coords/${area}/${fence}.route
+echo "*****Done with: ${fence}*****"
+date
 
 #### raleigh
 ## Map desired config into container
