d291ef94571a741ae8557fa5dc1073811f8ce455
mattlesnake <matt.ym.lee@gmail.com>
2022-08-06 10:05:57 -0400

    add asheboro to the mix

--- parallel.sh
+++ parallel.sh
@@ -2,9 +2,18 @@
 # Automate the routing process. Pass straight to sort
 # NOTE: Docker container that actually runs the scripts must already be built
 
-nohup ./cab.sh &> nohup-cab.txt &
-nohup ./cam.sh &> nohup-cam.txt &
-nohup ./cha.sh &> nohup-cha.txt &
-#nohup ./durham.sh &> nohup-durham.txt &
-nohup ./gso.sh &> nohup-gso.txt &
+
+
+nohup ./ash.sh &> nohup-ash.txt &
+sleep 1
 #nohup ./ral.sh &> nohup-ral.txt &
+#sleep 1
+#nohup ./gso.sh &> nohup-gso.txt &
+#sleep 1
+nohup ./cab.sh &> nohup-cab.txt &
+sleep 1
+#nohup ./cha.sh &> nohup-cha.txt &
+#sleep 1
+nohup ./cam.sh &> nohup-cam.txt &
+sleep 1
+nohup ./durham.sh &> nohup-durham.txt &
